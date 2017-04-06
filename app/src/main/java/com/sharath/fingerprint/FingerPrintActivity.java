d9e0560e318c0b7ad9f68a95a3843f2693b9b08a
YakseeHyd <suresh@yaksee.com>
2017-04-06 14:28:16 +0530

    Added all the project related files

---
+++ app/src/main/java/com/sharath/fingerprint/FingerPrintActivity.java
@@ -0,0 +1,13 @@
+package com.sharath.fingerprint;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FingerPrintActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finger_print);
+    }
+}
